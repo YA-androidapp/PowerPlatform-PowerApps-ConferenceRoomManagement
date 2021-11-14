4554df20e850ff202c6e0226cd88a111bfa22bb1
YA <YA-androidapp@users.noreply.github.com>
2021-11-14 12:19:19 +0900

    Create SetFloornumber.ps1

---
+++ PowerShell/SetFloornumber.ps1
@@ -0,0 +1,35 @@
+Install-Module -Name ExchangeOnlineManagement
+Import-Module ExchangeOnlineManagement
+
+
+Connect-ExchangeOnline -UserPrincipalName admin@contoso.com
+
+# https://docs.microsoft.com/ja-jp/powershell/module/exchange/get-mailbox
+#     $mailboxes = Get-Mailbox | Where-Object {($_.RecipientTypeDetails -eq "RoomMailbox") -or ($_.RecipientTypeDetails -eq "EquipmentMailbox")}
+$mailboxes = Get-Mailbox | Where-Object {$_.RecipientTypeDetails -eq "RoomMailbox"}
+$rooms = $mailboxes | ForEach-Object{Get-Place -Identity $_.PrimarySmtpAddress}
+$rooms | Select-Object Identity,DisplayName,Floor,FloorLabel | Sort-Object PrimarySmtpAddress | Format-List
+
+
+
+# https://docs.microsoft.com/ja-jp/powershell/module/exchange/set-place
+#     Set-Place -Identity room0201@yappf.onmicrosoft.com -Floor 2 -FloorLabel "2F"
+$mailboxes = Get-Mailbox | Where-Object {$_.RecipientTypeDetails -eq "RoomMailbox"}
+
+$mailboxes | Where-Object {$_.PrimarySmtpAddress.StartsWith("room02") } | ForEach-Object{Write-Host $_.PrimarySmtpAddress}
+$mailboxes | Where-Object {$_.PrimarySmtpAddress.StartsWith("room02") } | ForEach-Object{Set-Place -Identity $_.PrimarySmtpAddress -Floor 12 -FloorLabel "12F"}
+
+$mailboxes | Where-Object {$_.PrimarySmtpAddress.StartsWith("room12") } | ForEach-Object{Write-Host $_.PrimarySmtpAddress}
+$mailboxes | Where-Object {$_.PrimarySmtpAddress.StartsWith("room12") } | ForEach-Object{Set-Place -Identity $_.PrimarySmtpAddress -Floor 12 -FloorLabel "12F"}
+
+
+
+# https://docs.microsoft.com/ja-jp/powershell/module/exchange/new-distributiongroup
+$groupName = '全会議室'
+$groupAlias = 'AllRooms'
+New-DistributionGroup -Name $groupName -Alias $groupAlias -RoomList
+Get-DistributionGroup | Select-Object DisplayName,PrimarySmtpAddress | Sort-Object PrimarySmtpAddress | Format-List
+
+$mailboxes | ForEach-Object{ Add-DistributionGroupMember $groupAlias -Member $_.PrimarySmtpAddress }
+
+Get-DistributionGroupMember $groupAlias
